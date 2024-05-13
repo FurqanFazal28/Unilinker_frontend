5689c8e5c1d9e1ab2de858a42be623a1aa63e096
FurqanFazal28 <furqan.fazal444@gmail.com>
2024-05-13 03:20:41 -0700

    Modules Integration

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Stepper from './Components/Stepper';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Question from './Components/Question';
 import Result from './Components/Result';
+import UniversityFilter from './Components/UniversityFilter'
 import { ContextProvider } from './Hooks/UseStateContext'; 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/consultant" element={<Stepper />} />
           <Route path="/question" element={<Question />} />
           <Route path="/result" element={<Result />} />
+          <Route path='/unifilter' element={<UniversityFilter />} />
         </Routes>
       </ContextProvider>
     </BrowserRouter>
